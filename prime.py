42484855d18fcfd27ebf82f7cde2f231c940f094
Elizabeth Gomez <laidriana@gmail.com>
2025-06-05 09:22:21 -0600

    Limpieza de errores del código Python imprime los números primos menores de 100

--- prime.py
+++ prime.py
@@ -1,22 +1,23 @@
 import math
 
-def is_prime(n):
 
+def is_prime(n):
+    """Check if a number is prime."""
     if n <= 1:
         return False
-    for i in range(2,int(math.sqrt(n) + 1)):
+    for i in range(2, int(math.sqrt(n) + 1)):
         if n % i == 0:
             return False
     return True
 
+
 def main():
-
-    """tiene toda la lógica principal"""
-
+    """Main logic to print prime numbers less than 100."""
     for i in range(100):
         if is_prime(i):
-            print (i, end=' ')
-    print();
+            print(i, end=' ')
+    print()
+
 
 if __name__ == '__main__':
     main()
